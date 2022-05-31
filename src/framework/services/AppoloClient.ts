f73549500763385af7d5f34aad9251a7551ca522
Aleksandra Niewińska <fruzia@desmart.com>
2022-05-31 15:03:46 +0200

    feat: add token

--- src/framework/services/AppoloClient.ts
+++ src/framework/services/AppoloClient.ts
@@ -1,6 +1,11 @@
 import {ApolloClient, InMemoryCache} from '@apollo/client';
 
+const authToken =
+  'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MzMzLCJ0eXBlIjoiQUNDRVNTIiwiaWF0IjoxNjU0MDAxODUzLCJleHAiOjE2NTQ2MDY2NTN9.nGJ6pHjFlVcMYuZxwQ4gKqxcl2fjMULytIYBjGVtn57xHXkEL4isTSqUsXcTbaajhZTc_gtdoWmSjU5VUStcmmMiVj7P0uAOhSVc4WX-Xl8kn3MSkTvTOT1Ah4IIEFBB3IjKTt7GcH83awd43WnIZ8BpFYnDltFsAVPC-vPcwxpJD3BEbFBwQXcsI80lZlVKddQhJtCB_MGFz0XKCCR1sol-lxoBD69HguJF8QWhRA4K-Lg1bbCgkoikZyMNGVPQkdClgdLXLmBalcvikPgM9Ta7c76Hf390z8Pz9NO_9DI3c_3_hrhrsucpZdxzSQEGg8Xj14SMpZ1i-Pcdgf6Hgg';
+const API = 'https://api-dev.foodstyles.com/graphql';
+
 export const client = new ApolloClient({
-  uri: 'https://api-dev.foodstyles.com/graphql',
+  uri: API,
+  headers: {Authorization: `${authToken}`},
   cache: new InMemoryCache(),
 });
