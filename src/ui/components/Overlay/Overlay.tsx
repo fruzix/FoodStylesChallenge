5686eb1c27ecc89f95df090a2f0ee5f531d39580
Aleksandra Niewińska <fruzia@desmart.com>
2022-05-31 16:24:33 +0200

    chore: create Blur effect

---
+++ src/ui/components/Overlay/Overlay.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {ViewStyle, useWindowDimensions} from 'react-native';
+import {BlurView} from '@react-native-community/blur';
+
+import useStyle from '@Framework/hooks/useStyle';
+
+interface IOverlayStyle {
+  container: ViewStyle;
+}
+
+const Overlay = () => {
+  const {width, height} = useWindowDimensions();
+
+  const styles = useStyle<IOverlayStyle>(
+    {
+      container: {
+        position: 'absolute',
+        width,
+        height,
+        zIndex: 5,
+      },
+    },
+    [width, height],
+  );
+
+  return (
+    <BlurView
+      style={styles.container}
+      blurType="light"
+      blurAmount={5}
+      reducedTransparencyFallbackColor="white"
+    />
+  );
+};
+
+export default Overlay;
