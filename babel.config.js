4d46fb52a19226b36d593080e54e61ab21283f86
Aleksandra Niewińska <fruzia@desmart.com>
2022-05-31 10:21:44 +0200

    feat: setup ts config

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,23 @@
+const tsconfig = require('./tsconfig.json');
+
+let rawAlias = tsconfig.compilerOptions.paths;
+let alias = {};
+
+for (let x in rawAlias) {
+  alias[x.replace('/*', '')] = rawAlias[x].map(p => p.replace('/*', ''));
+}
+
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        cwd: 'babelrc',
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias,
+      },
+    ],
+    'jest-hoist',
+  ],
 };
