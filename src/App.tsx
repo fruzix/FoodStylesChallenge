69664ed29fc4a6a49bdc1f08f31da98ebc100081
Aleksandra Niewińska <fruzia@desmart.com>
2022-05-31 10:24:04 +0200

    chore: move App to src

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import AppNavigator from '@Framework/navigation/AppNavigator';
+import {NavigationContainer} from '@react-navigation/native';
+import React from 'react';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+
+const App = () => {
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <AppNavigator />
+      </NavigationContainer>
+    </SafeAreaProvider>
+  );
+};
+
+export default App;
