c0e7b83d5e79d67e4ada192037ef8aaeed8ad6de
Aleksandra Niewińska <fruzia@desmart.com>
2022-05-31 10:35:28 +0200

    feat: setup redux store

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
+import {store} from '@App/store';
 import AppNavigator from '@Framework/navigation/AppNavigator';
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {Provider} from 'react-redux';
 
 const App = () => {
   return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <AppNavigator />
-      </NavigationContainer>
-    </SafeAreaProvider>
+    <Provider store={store}>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <AppNavigator />
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </Provider>
   );
 };
 
