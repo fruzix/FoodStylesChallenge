75793939ec1e657f54242bb1cc6444f4d4d3cf01
Aleksandra Niewińska <fruzia@desmart.com>
2022-05-31 11:28:56 +0200

    feat: create theme

---
+++ src/framework/hooks/useTheme.ts
@@ -0,0 +1,11 @@
+import {Theme} from '@Ui/Theme/theme';
+
+const useAppTheme = () => {
+  // const scheme = useColorScheme();
+  //
+  // return scheme === 'dark' ? LightTheme : DarkTheme;
+
+  return Theme;
+};
+
+export default useAppTheme;
