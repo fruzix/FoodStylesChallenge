9cbdd3bdd980bf4f1bb20dfa00df2c54c8fab64c
Aleksandra Niewińska <fruzia@desmart.com>
2022-05-31 18:57:39 +0200

    chore: move token and api string to constants

--- src/framework/services/AppoloClient.ts
+++ src/framework/services/AppoloClient.ts
@@ -1,11 +1,8 @@
 import {ApolloClient, InMemoryCache} from '@apollo/client';
-
-const authToken =
-  'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MzMzLCJ0eXBlIjoiQUNDRVNTIiwiaWF0IjoxNjU0MDAxODUzLCJleHAiOjE2NTQ2MDY2NTN9.nGJ6pHjFlVcMYuZxwQ4gKqxcl2fjMULytIYBjGVtn57xHXkEL4isTSqUsXcTbaajhZTc_gtdoWmSjU5VUStcmmMiVj7P0uAOhSVc4WX-Xl8kn3MSkTvTOT1Ah4IIEFBB3IjKTt7GcH83awd43WnIZ8BpFYnDltFsAVPC-vPcwxpJD3BEbFBwQXcsI80lZlVKddQhJtCB_MGFz0XKCCR1sol-lxoBD69HguJF8QWhRA4K-Lg1bbCgkoikZyMNGVPQkdClgdLXLmBalcvikPgM9Ta7c76Hf390z8Pz9NO_9DI3c_3_hrhrsucpZdxzSQEGg8Xj14SMpZ1i-Pcdgf6Hgg';
-const API = 'https://api-dev.foodstyles.com/graphql';
+import {API, AUTH_TOKEN} from '@App/constants';
 
 export const client = new ApolloClient({
   uri: API,
-  headers: {Authorization: `${authToken}`},
+  headers: {Authorization: `${AUTH_TOKEN}`},
   cache: new InMemoryCache(),
 });
